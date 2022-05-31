04ae0d666d486b72bb05fb122b7c189eb1cbbc87
Caio <caiopacnunes@gmail.com>
2022-05-31 14:49:27 -0300

    Primeiro exercício

---
+++ JAVA/ex01/main.java
@@ -0,0 +1,27 @@
+package ex01;
+
+public class main {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Exercícios calculadora");
+        calculadora.soma(5, 11);
+        calculadora.subtracao(12, 232);
+        calculadora.multiplicacao(1234, 34);
+        calculadora.divisao(232, 243);
+
+        System.out.println("Exercícios mensagem");
+        mensagem.obterMensagem(8);
+        mensagem.obterMensagem(13);
+        mensagem.obterMensagem(20);
+    
+        System.out.println("Exercício Empréstimo");
+        emprestimo.calcular(1000, 2);
+        emprestimo.calcular(1000, 3);
+        emprestimo.calcular(1000, 5);
+        
+
+
+
+    }
+}
